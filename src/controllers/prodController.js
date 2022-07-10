4370a5f142e2a9cdab44546659f9da5ee4ae9878
ninaschramm <schrammnina@outlook.com>
2022-07-10 19:48:47 -0300

    getting one product

--- src/controllers/prodController.js
+++ src/controllers/prodController.js
@@ -4,3 +4,8 @@ export async function getProducts(req, res){
     const productList = await db.collection("products").find().toArray();        
     return res.send(productList);
 }
+
+export async function getOneProduct(req, res){
+    const id = req.params.id
+    const product = await db.collection("products").find( {id: id} ).toArray();
+}
--- src/routes/prodRouter.js
+++ src/routes/prodRouter.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getProducts } from '../controllers/prodController.js';
+import { getProducts, getOneProduct } from '../controllers/prodController.js';
 
 const prodRouter = Router();
 
 prodRouter.get('/products', getProducts); 
+prodRouter.get('/products/:id', getOneProduct); 
+
 
 export default prodRouter;
