9dc963934f756907c5bab6dc35b3454bfadb8622
Augusto Lopes <augustolfp@gmail.com>
2022-07-11 16:01:35 -0300

    Refactors routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import navigationRouter from "./navigationRouter.js";
 import authRouter from './authRouter.js';
+import prodRouter from "./prodRouter.js";
 
 const router = Router();
 router.use(navigationRouter);
 router.use(authRouter);
+router.use(prodRouter);
 
 
 export default router;
