6df9163af3648f92b8cf3e1b7a4da8acf47d7a38
ninaschramm <schrammnina@outlook.com>
2022-07-10 23:18:21 -0300

    navigation controllers for shopping cart

--- src/controllers/navigationController.js
+++ src/controllers/navigationController.js
@@ -1,4 +1,30 @@
-export async function getShoppingCart(req, res) {
+import db from '../dbStrategy/mongo.js';
+
+export async function getCart(req, res){
     const user = res.locals.user;
-    res.status(200).send(`olá, ${user.name}! Você está logado, e essa é a página com o seu carrinho de compras!`);
+    const myCart = await db.collection("carts").find({id: user._id}).toArray();        
+    return res.send(myCart);
+}
+
+export async function updateCart(req, res){
+    const user = res.locals.user;
+    const product = req.body
+    try {
+        await db.collection("cart").insertOne( {...product, userId: user._id}  );
+        return res.send(product)
+    }
+    catch(err) {
+        return res.send("Algo deu errado!")
+    }
+}
+
+export async function clearCart(req, res) {
+    const user = res.locals.user;
+    try {
+        await db.collection("cart").deleteMany( { id: user._id} );
+        return res.sendStatus(200)
+    }
+    catch(err){
+        return res.send("Algo deu errado!")
+    }
 }
--- src/routes/navigationRouter.js
+++ src/routes/navigationRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getShoppingCart } from "../controllers/navigationController.js";
+import { getCart, updateCart, clearCart } from "../controllers/navigationController.js";
 import validateToken from "../middlewares/authCheckMiddleware.js";
 
 const navigationRouter = Router();
 
-navigationRouter.get("/carrinho", validateToken, getShoppingCart);
+navigationRouter.get("/carrinho", validateToken, getCart);
+navigationRouter.post("/carrinho", validateToken, updateCart);
+navigationRouter.post("/carrinho", validateToken, clearCart);
+
 
 export default navigationRouter;
